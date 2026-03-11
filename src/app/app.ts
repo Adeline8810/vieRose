13db364b85a843e3b7f7af08b2d0ff839bbf0e2b
Ruth <ruthmarilsa20@gmail.com>
2026-03-11 17:14:17 +0100

    Guardando cambios finales en rama borrador

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,12 +6,13 @@ import { Navbar } from './components/navbar/navbar';
 import { Hero } from './components/hero/hero';
 import { Features } from './components/features/features';
 import { ProductGrid} from './components/product-grid/product-grid';
+import { TopBar } from './components/top-bar/top-bar';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar,Hero,Features,ProductGrid],
+  imports: [Navbar,Hero,Features,ProductGrid,TopBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
