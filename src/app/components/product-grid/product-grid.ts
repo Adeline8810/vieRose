e9eb2322b03b4ad35d7afff4d009c77c68c6f84d
Ruth <ruthmarilsa20@gmail.com>
2026-03-12 10:36:23 +0100

    agregando imagenes

--- src/app/components/product-grid/product-grid.ts
+++ src/app/components/product-grid/product-grid.ts
@@ -8,4 +8,17 @@ import { CommonModule } from '@angular/common';
   templateUrl: './product-grid.html',
   styleUrl: './product-grid.css'
 })
-export class ProductGrid {}
+export class ProductGrid {
+
+productos = [
+  { nombre: 'Robe Romantique', precio: '89.99€', imagen: 'assets/images/imagen1.jfif' },
+  { nombre: 'Robe Dentelle', precio: '75.00€', imagen: 'assets/images/imagen2.png' },
+  { nombre: 'Robe Dentelle', precio: '85.00€', imagen: 'assets/images/imagen3.png' },
+  { nombre: 'Robe Dentelle', precio: '95.00€', imagen: 'assets/images/imagen4.jfif' },
+  // ... agrega todos los que quieras
+];
+
+
+}
+
+
